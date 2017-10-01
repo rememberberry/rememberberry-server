f64f12c12c1c7c94e3d007a969af68a29f6eb003
Martin Pettersson <martin@rememberberry.com>
2017-10-01 15:58:38 +0200

    Fix fuse not working with osx host

--- run_docker_stage.sh
+++ run_docker_stage.sh
@@ -1,1 +1,2 @@
-docker run -p 443:443 -p 80:80 -v ~/.ipfs:/root/.ipfs -v $(pwd):/rememberberry-server-stage --entrypoint=/bin/bash -ti rememberberry
+# Note: need the "privileged" option for fuse to work on osx
+docker run -p 443:443 -p 80:80 -v ~/.ipfs:/.ipfs -v $(pwd):/rememberberry-server-stage --privileged --entrypoint=/bin/bash -ti rememberberry

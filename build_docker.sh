729c05f588141186bc7d5c4e77dd2130182f06e8
Martin Pettersson <martin@rememberberry.com>
2018-01-03 19:47:59 +0100

    Create an empty certs folder if cp failed

--- build_docker.sh
+++ build_docker.sh
@@ -2,4 +2,7 @@
 # support symlinks
 rm -rf docker/certs
 cp -Lr /etc/letsencrypt/live/bot.rememberberry.com docker/certs
+if [ ! -f docker/certs ]; then
+  mkdir docker/certs
+fi
 docker build --build-arg CACHE_DATE="$(date)" -t rememberberry docker/
